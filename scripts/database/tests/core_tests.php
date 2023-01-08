b7b2f59a2463d982a5a6a83a0615f528442ec5c8
lrshsl <hoeslilars@gmail.com>
2023-01-08 13:08:41 +0100

    add index.html and basic test setup

---
+++ scripts/database/tests/core_tests.php
@@ -0,0 +1,19 @@
+// Copyright 2023 lrshsl.
+// Use of this source code is governed by the WTFPL
+// license that can be found in the LICENSE file.
+
+
+<?php
+
+// Database is required
+require "../create.php";
+
+
+$core_tests = [
+    function($db) {
+        // First reset the database
+    }
+]
+
+
+?>
